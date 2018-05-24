d451afc69479e2d1b4cafcfa3d82afa370a7f8f0
Kiritow <1362050620@qq.com>
2018-05-24 17:33:08 +0800

    Add VM

--- vmop.cpp
+++ vmop.cpp
@@ -1,2 +1,33 @@
 #include "vmop.h"
+using namespace std;
 
+VM::VM()
+{
+	_luavm = luaL_newstate();
+	luaL_openlibs(_luavm);
+	runCode("response={}");
+}
+
+
+VM::~VM()
+{
+	lua_close(_luavm);
+}
+
+int VM::runCodeEx(const function<void(lua_State*)>& prepare,
+	const function<void(lua_State*)>& after,
+	const string& LuaSource)
+{
+	bool err = luaL_loadbuffer(_luavm, LuaSource.c_str(), (int)LuaSource.size(), "LuaVM") || lua_pcall(_luavm, 0, 0, 0);
+	if (err) {
+		printf("%s\n", lua_tostring(_luavm, -1));
+		lua_pop(_luavm, 1);
+		return -1;
+	}
+	return 0;
+}
+
+int VM::runCode(const string& LuaSource)
+{
+	return runCodeEx([](lua_State*) {}, [](lua_State*) {}, LuaSource);
+}
--- vmop.h
+++ vmop.h
@@ -1,7 +1,24 @@
 #pragma once
+#include <string>
+#include <functional>
 #include "LuaSrc/include/lua.hpp"
 
 class VM
 {
+public:
+	VM();
+	/// NonMoveable,NonCopyable
+	VM(const VM&) = delete;
+	VM& operator = (const VM&) = delete;
+	VM(VM&&) = delete;
+	VM& operator = (VM&&) = delete;
+	~VM();
 
+	int runCodeEx(const std::function<void(lua_State*)>& prepare, 
+		const std::function<void(lua_State*)>& after,
+		const std::string& LuaSource);
+
+	int runCode(const std::string& LuaSource);
+private:
+	lua_State * _luavm;
 };
