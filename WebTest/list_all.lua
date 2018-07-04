f939af41786d0ec4b9d3b7786e3c48946c4a6289
Kiritow <1362050620@qq.com>
2018-07-04 21:07:31 +0800

    Update WebTests

--- WebTest/list_all.lua
+++ WebTest/list_all.lua
@@ -1,4 +1,12 @@
 helper.print("<h1>Hello World!</h1>")
 for k,v in pairs(request) do
-    helper.print("<p><b>" .. k .. "</b>\t" .. v .. "</p>")
+    pcall(function() helper.print("<p><b>" .. k .. "</b>\t" .. v .. "</p>") end)
+end
+helper.print("<h2>Request Parameters</h2>")
+if(request.method=="GET") then
+    for k,v in pairs(request.param) do
+        pcall(function() helper.print("<p><b>" .. k .. "</b>\t" .. v .. "</p>") end)
+    end
+else
+    pcall(function() helper.print("<p>" .. request.param .. "</p>") end)
 end
