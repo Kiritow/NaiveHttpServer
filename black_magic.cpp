c0768a8d12bfdb8b1035d80be40fcf0fb2609aff
Kiritow <1362050620@qq.com>
2018-09-10 15:45:06 +0800

    Update non-blocking code

--- black_magic.cpp
+++ black_magic.cpp
@@ -13,8 +13,14 @@ int black_magic(serversock& t)
 char exbuff[10240];
 int black_magic(serversock& t)
 {
+	if (t.setNonblocking() < 0)
+	{
+		loge("Failed to set serversocket to non-blocking\n");
+		return -1;
+	}
+
 	epoll ep(10240);
-	ep.add(t, EPOLLIN);
+	ep.add(t, EPOLLIN | EPOLLET | EPOLLERR);
 
 	map<vsock*, string> mp;
 
@@ -26,44 +32,107 @@ int black_magic(serversock& t)
 			loge("epoll error with ret: %d. errno: %d\n", ret, errno);
 			break;
 		}
+		bool stop_server = false;
 		// Handle events
 		ep.handle([&](vsock& v, int event) {
+			logd("epoll handle: vsock %p event %d\n", &v, event);
 			if (&v == &t)
 			{
-				// ServerSocket is readable. We can call accept() on it.
-				sock* ps = new sock;
-				if (t.accept(*ps) <= 0)
+				if (event & EPOLLIN)
 				{
-					loge("Failed to accept.\n");
+					// ServerSocket is readable. We can call accept() on it.
+					sock* ps = new sock;
+					auto accres = t.accept_nb(*ps);
+					if (!accres.isFinished())
+					{
+						loge("Failed to finish accept.\n");
+					}
+					else if (!accres.isSuccess())
+					{
+						loge("Accept is failed.\n");
+					}
+					else
+					{
+						if (ps->setNonblocking() < 0)
+						{
+							loge("Failed to set client socket to non-blocking. %p\n", ps);
+							delete ps;
+						}
+						else
+						{
+							logd("New connection accepted. Adding %p to epoll.\n", ps);
+							ep.add(*ps, EPOLLIN | EPOLLET | EPOLLERR);
+						}
+					}
 				}
-				else
+				else if (event & EPOLLERR)
 				{
-					logd("Adding %p to epoll.\n", ps);
-					ep.add(*ps, EPOLLIN);
+					// Server socket is error. Stop Server.
+					stop_server = true;
 				}
 			}
 			else
 			{
-				// Socket is readable. Read it.
 				sock& s = (sock&)v;
-				memset(exbuff, 0, 10240);
-				int ret = s.recv(exbuff, 10240);
-				if (ret <= 0)
+				if (event & EPOLLIN)
 				{
-					loge("Failed to read. Removing from epoll.\n");
-					ep.del(v, EPOLLIN);
-				}
-				else
-				{
-					mp[&v].append(string(exbuff, ret));
-					if (isHttpHeader(mp[&v]))
+					// Socket is readable. Read it
+					memset(exbuff, 0, 10240);
+					auto recres = s.recv_nb(exbuff, 10240);
+					recres.setStopAtEdge(true);
+					if (!recres.isFinished())
 					{
-						request_handler_main(s,mp[&v]);
-						logd("HTTP handle finished. removing from epoll...\n");
-						ep.del(v, EPOLLIN);
-						mp.erase(&v);
+						loge("Failed to finish recv. Removing from epoll and releasing resource... %p\n", &s);
+						mp.erase(&s);
+						ep.del(s, EPOLLIN | EPOLLET | EPOLLERR);
 						delete &s;
 					}
+					else if (!recres.isSuccess())
+					{
+						if (recres.getErrCode() == gerrno::WouldBlock)
+						{
+							// No more data yet
+							mp[&s].append(string(exbuff, recres.getBytesDone()));
+							if (isHttpHeader(mp[&s]))
+							{
+								// This is http header.
+								request_handler_main(s, mp[&s]);
+								logd("HTTP handle finished. removing from epoll and releasing resource... %p\n", &s);
+								mp.erase(&s);
+								ep.del(s, EPOLLIN | EPOLLET | EPOLLERR);
+								delete &s;
+							}
+						}
+						else
+						{
+							loge("Recv is Failed. Removing from epoll and releasing resource... %p\n", &s);
+							mp.erase(&s);
+							ep.del(s, EPOLLIN | EPOLLET | EPOLLERR);
+							delete &s;
+						}
+					}
+					else
+					{
+						// Here is a little bug, if request header is more than 10240 and mode is ET, then we will infinitely hang up here.
+						// Finished, Success
+						mp[&s].append(string(exbuff, recres.getBytesDone()));
+						if (isHttpHeader(mp[&s]))
+						{
+							// This is http header.
+							request_handler_main(s, mp[&s]);
+							logd("HTTP handle finished. removing from epoll and releasing resource... %p\n", &s);
+							ep.del(s, EPOLLIN | EPOLLET | EPOLLERR);
+							mp.erase(&s);
+							delete &s;
+						}
+					}
+				}
+				else if (event & EPOLLERR)
+				{
+					// Socket is error.
+					loge("Socket is error. Removing from epoll and releasing resource... %p\n", &s);
+					mp.erase(&s);
+					ep.del(s, EPOLLIN | EPOLLET | EPOLLERR);
 				}
 			}
 		});
